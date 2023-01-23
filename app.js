03095534bc36e94f429d65cd22f9520eac5f98e3
nashEm8 <emanuel82022@outlook.com>
2023-01-22 23:41:44 -0300

    Resolvendo exercício 3

--- app.js
+++ app.js
@@ -1,12 +1,61 @@
-"use strict";
-const firstInput = document.querySelector("#input1");
-const secondInput = document.querySelector("#input2");
-const button = document.querySelector("button");
-const calcular = (firstNumber, secondNumber) => {
-    return firstNumber + secondNumber;
+//Exercício 01 
+var employee = {
+    code: 10,
+    name: 'John'
 };
-if (button) {
-    button.addEventListener("click", () => {
-        console.log(calcular(Number(firstInput.value), Number(secondInput.value)));
-    });
+employee.code = 10;
+employee.name = "John";
+/*
+  Exercício 02
+  Como podemos melhorar o esse código usando TS?
+*/
+var Trabalho;
+(function (Trabalho) {
+    Trabalho[Trabalho["Atriz"] = 0] = "Atriz";
+    Trabalho[Trabalho["Padeiro"] = 1] = "Padeiro";
+})(Trabalho || (Trabalho = {}));
+var pessoa1 = {
+    nome: "maria",
+    idade: 29,
+    profissao: Trabalho.Atriz
+};
+var pessoa2 = {
+    nome: "roberto",
+    idade: 35,
+    profissao: Trabalho.Padeiro
+};
+var pessoa3 = {
+    nome: "laura",
+    idade: 28,
+    profissao: Trabalho.Atriz
+};
+var pessoa4 = {
+    nome: "carlos",
+    idade: 15,
+    profissao: Trabalho.Padeiro
+};
+/* Exercício 03 */
+// O código abaixo tem alguns erros e não funciona como deveria. Você pode identificar quais são e corrigi-los em um arquivo TS?
+var botaoAtualizar = document.getElementById('atualizar-saldo');
+var botaoLimpar = document.getElementById('limpar-saldo');
+var soma = document.getElementById('soma');
+var campoSaldo = document.getElementById('campo-saldo');
+campoSaldo.innerHTML = 0;
+function somarAoSaldo(soma) {
+    campoSaldo.innerHTML += soma;
 }
+function limparSaldo() {
+    campoSaldo.innerHTML = '';
+}
+botaoAtualizar.addEventListener('click', function () {
+    somarAoSaldo(soma.value);
+});
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
+/**
+    <h4>Valor a ser adicionado: <input id="soma"> </h4>
+    <button id="atualizar-saldo">Atualizar saldo</button>
+    <button id="limpar-saldo">Limpar seu saldo</button>
+    <h1>"Seu saldo é: " <span id="campo-saldo"></span></h1>
+ */ 
--- app.ts
+++ app.ts
@@ -51,26 +51,29 @@ let pessoa4 : Dice = {
 /* Exercício 03 */
 // O código abaixo tem alguns erros e não funciona como deveria. Você pode identificar quais são e corrigi-los em um arquivo TS?
 
-let botaoAtualizar = document.getElementById('atualizar-saldo') as HTMLBodyElement;
-let botaoLimpar = document.getElementById('limpar-saldo') as HTMLBodyElement;
-let soma = document.getElementById('soma') as HTMLElement;
-let campoSaldo = document.getElementById('campo-saldo') as HTMLElement;
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo')!;
+let soma = document.getElementById('soma')! as HTMLInputElement;
+let campoSaldo = document.getElementById('campo-saldo');
 
-campoSaldo.innerHTML = 0;
+let saldo = 0;
 
-function somarAoSaldo(soma) {
-    campoSaldo.innerHTML += soma;
+function somarAoSaldo(soma: number) {
+    if(campoSaldo){
+      soma.value += saldo
+      campoSaldo.innerHTML = soma.toString();
+    }
 }
 
 function limparSaldo() {
-    campoSaldo.innerHTML = '';
+    soma.value = '';
 }
 
-botaoAtualizar.addEventListener('click', function () {
-    somarAoSaldo(soma.value);
+botaoAtualizar?.addEventListener('click', function () {
+    somarAoSaldo(Number(soma.value));
 });
 
-botaoLimpar.addEventListener('click', function () {
+botaoLimpar?.addEventListener('click', function () {
     limparSaldo();
 });
 
